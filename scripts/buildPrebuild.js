993f15e5ee1d7ebc4df8df68b4589429c5c23ca3
Jared Casey <thejcfactor@users.noreply.github.com>
2026-05-12 10:09:26 -0600

    JSCO-62: Move build scripts to use strict mode (#40)

--- scripts/buildPrebuild.js
+++ scripts/buildPrebuild.js
@@ -15,6 +15,8 @@
  *  limitations under the License.
  */
 
+'use strict'
+
 const prebuilds = require('./prebuilds')
 const os = require('os')
 
--- scripts/install.js
+++ scripts/install.js
@@ -15,6 +15,8 @@
  *  limitations under the License.
  */
 
+'use strict'
+
 const path = require('path')
 const prebuilds = require('./prebuilds')
 
